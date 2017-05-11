065c520a4da8678e4246ec184393083ce6e08fc2
pekko1215 <pekko1215@yahoo.co.jp>
2017-05-11 14:25:35 +0900

    ChromeからCommit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var iconv = require('iconv');
 
 var conv = new iconv.Iconv('SJIS', 'UTF-8//TRANSLIT//IGNORE');
 
+var slicer = 'PachiSlot'
+
 var ps = new shell({
         executionPolicy: 'Bypass',
         noProfile: false
@@ -11,37 +13,57 @@ var ps = new shell({
 var server = ws.listen(5000, function() {
         console.log('Server running');
 })
-
+var bufferArray = [];
+var pwdFlag = false;
 // クライアントからの接続
 // イベントを処理
 server.on('connection', function(client) {
         console.log('connection start');
-        sendPath(client);
         ps.streams.stderr.on('data', function(data) {
                 client.send(JSON.stringify({
                         'err': data
                 }))
         })
         ps.streams.stdout.on('data', function(data) {
-                if (data.indexOf('EOI') !== -1) {
-                        client.send(JSON.stringify({
-                                'std': data
-                        }))
+                var arr = data.split("\n");
+                arr.splice(arr.length - 1, 1);
+                if (pwdFlag) {
+                        sendPath(arr[0], client)
+                        arr.splice(0, 1);
+                        pwdFlag = false;
                 } else {
-                        sendPath(client)
-                        console.log("しゅうりょうかくにん")
+                        for (var i = 0; i < arr.length; i++) {
+                                console.log("arr[" + i + "] = " + arr[i])
+                                if (arr[i].indexOf(slicer) == 0) {
+                                        if (arr.length <= i + 1) {
+                                                arr.splice(i, 1);
+                                                pwdFlag = true;
+                                                break;
+                                        } else {
+				     arr.splice(i,1);
+                                                sendPath(arr[i], client)
+                                                arr.splice(i, 1);
+                                                break;
+                                        }
+                                }
+                        }
                 }
+                // console.log(arr)
+                client.send(JSON.stringify({
+                        'std': arr.join('\n')
+                }))
         })
         ps.streams.stdout.on('end', function(data) {
                 client.send(JSON.stringify({
                         'std': data
                 }))
-                sendPath(client)
+                console.log('end' + data)
         })
+
         client.on('message', function(request) {
                 // ps.addCommand(request)
                 // ps.invoke()
-                ps.streams.stdin.write(request + ";echo EOI\n");
+                ps.streams.stdin.write(request + ";echo " + slicer + ";echo $pwd.Path\n");
         });
 
         // クライアントが切断したときの処理
@@ -59,18 +81,14 @@ server.on('connection', function(client) {
                 console.log(err);
                 console.log(err.stack);
         });
+
+        ps.streams.stdin.write("echo " + slicer + ";echo $pwd.Path\n")
 });
 
-function sendPath(client) {
-        ps.addCommand('pwd');
-        ps.invoke()
-                .then(function(output) {
-                        var obj = JSON.stringify({
-                                'path': ((function(arr) {
-                                        return arr.shift().match(/----/) ? arr.shift() : arguments.callee(arr)
-                                })(output.split("\n")))
-                        })
-                        console.log(obj)
-                        client.send(obj)
-                })
+function sendPath(output, client) {
+        console.log("output:" + output)
+        var obj = JSON.stringify({
+                'path': output
+        });
+        client.send(obj)
 }
